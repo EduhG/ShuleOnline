a0eddb5ef772b7fccdbe14ff935c8b8f3a34904d
EduhG <edwinnyangena@gmail.com>
2016-08-28 17:53:35 +0300

    Created student object

---
+++ src/studentManagement/student.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentManagement;
+
+/**
+ *
+ * @author EduhG
+ */
+public class student {
+    private String admNo;
+    private String admDate;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String gender;
+    private String dateOfBirth;
+    private String birthCertNo;
+    private String form;
+    private String stream;
+    private String admType;
+
+    public String getAdmNo() {
+        return admNo;
+    }
+
+    public void setAdmNo(String admNo) {
+        this.admNo = admNo;
+    }
+
+    public String getAdmDate() {
+        return admDate;
+    }
+
+    public void setAdmDate(String admDate) {
+        this.admDate = admDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getBirthCertNo() {
+        return birthCertNo;
+    }
+
+    public void setBirthCertNo(String birthCertNo) {
+        this.birthCertNo = birthCertNo;
+    }
+
+    public String getForm() {
+        return form;
+    }
+
+    public void setForm(String form) {
+        this.form = form;
+    }
+
+    public String getStream() {
+        return stream;
+    }
+
+    public void setStream(String stream) {
+        this.stream = stream;
+    }
+
+    public String getAdmType() {
+        return admType;
+    }
+
+    public void setAdmType(String admType) {
+        this.admType = admType;
+    }
+    
+    
+}

9eb283ade0de36303239c2f2046f1c3561a2dd7a
EduhG <edwinnyangena@gmail.com>
2016-08-28 21:35:05 +0300

    added getters and setters to subjects

--- src/examManagement/subjectsManagement.java
+++ src/examManagement/subjectsManagement.java
@@ -15,4 +15,46 @@ public class subjectsManagement {
     public String cartegory;
     public String subcartegory;
     public int totalMarks;
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getCartegory() {
+        return cartegory;
+    }
+
+    public void setCartegory(String cartegory) {
+        this.cartegory = cartegory;
+    }
+
+    public String getSubcartegory() {
+        return subcartegory;
+    }
+
+    public void setSubcartegory(String subcartegory) {
+        this.subcartegory = subcartegory;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+    
+    
 }
